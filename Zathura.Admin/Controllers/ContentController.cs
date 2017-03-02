9670434162d73fb439a3d81de7e4a07aa45bd71b
tufanumut@gmail.com <tufanumut@gmail.com>
2017-03-02 17:56:57 +0300

    ckeditor integrated to panel

---
+++ Zathura.Admin/Controllers/ContentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zathura.Admin.CustomFilter;
+
+namespace Zathura.Admin.Controllers
+{
+    public class ContentController : Controller
+    {
+        
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [LoginFilter]
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
