11d6afda624ca7ccf3c05541b1af119e645cb7c7
Ersin Kaya <ersinkaya7@hotmail.com>
2018-02-14 22:55:54 +0300

    mobil ve web menü tamamlandı

--- Zathura.UI/Assets/src/js/main.js
+++ Zathura.UI/Assets/src/js/main.js
@@ -1,5 +1,16 @@
 ﻿$(document).ready(function () {
 
+    $(".mobile-menu").click(function () {
+        $(this).next(".main-menu").addClass("opened");
+    });
+
+    $(".close-menu").click(function () {
+        $(".main-menu").removeClass("opened"); 
+    });
+    
+    openSubMenu(".main-menu-item-wrapper", ".wide-menu");
+    openSubMenu(".opened .sub-menu-title", ".sub-menu");
+
     var mySwiper = new Swiper('.main-slider', {
         slidesPerView: 1,
         spaceBetween: 30,
@@ -15,6 +26,11 @@
         effect: 'fade'
     });
 
-    console.log("ersin");
 });
+
+function openSubMenu(clicked, opened) {
+    $(document).on("click", clicked, function () {
+        $(this).next(opened).toggleClass("opened");
+    });
+}
 
